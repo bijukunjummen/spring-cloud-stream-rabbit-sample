06b1055c22cb801b40605a5e9a3a7cbc3e3965a1
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-08-24 16:45:48 -0700

    Externalized some of the configuration

--- work-dispatcher/src/main/java/works/service/IntegrationConfiguration.java
+++ work-dispatcher/src/main/java/works/service/IntegrationConfiguration.java
@@ -1,11 +1,12 @@
 package works.service;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @Configuration
-@EnableBinding(WorkUnitsSource.class)
+@EnableBinding(Source.class)
 @IntegrationComponentScan
 public class IntegrationConfiguration {
 }
--- work-handler/src/main/java/works/processor/WorkHandler.java
+++ work-handler/src/main/java/works/processor/WorkHandler.java
@@ -2,13 +2,11 @@ package works.processor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
 import works.processor.domain.WorkUnit;
 
 @Service
-@EnableBinding(WorkUnitsSink.class)
 public class WorkHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkHandler.class);
 
