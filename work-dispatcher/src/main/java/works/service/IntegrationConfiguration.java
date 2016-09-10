b7617e41ea4045d5de7f27d2752b5fa45ce4a127
Philipp Bobek <philipp.bobek@web.de>
2016-09-10 15:46:52 +0200

    Bind to WorkUnitSource instead of standard Source

--- work-dispatcher/src/main/java/works/service/IntegrationConfiguration.java
+++ work-dispatcher/src/main/java/works/service/IntegrationConfiguration.java
@@ -1,12 +1,11 @@
 package works.service;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @Configuration
-@EnableBinding(Source.class)
+@EnableBinding(WorkUnitsSource.class)
 @IntegrationComponentScan
 public class IntegrationConfiguration {
 }
